655d229288e309d759ab04449c2da7ec2431109b
Francesco Terenzi <hfx923@qmul.ac.uk>
2023-11-01 15:38:48 +0000

    NTau 100000 with b0 1 and N 100000

--- hsc.sh
+++ hsc.sh
@@ -3,7 +3,7 @@
 #$ -l h_vmem=16G
 #$ -j y
 #$ -l h_rt=01:0:0
-#$ -t 1-40000
+#$ -t 1-50000
 #$ -o jobs/$JOB_NAME.$JOB_ID.$TASK_ID.out
 
 PATH2HSC=/data/home/hfx923/hsc
@@ -13,7 +13,7 @@ PATH2SAVE=/data/scratch/hfx923/hsc-draft/${VERSION}
 
 echo "run hsc seed ${SGE_TASK_ID} ${PATH2SAVE} $(date +"%T")"
 
-${PATH2HSC}/target/release/hsc -c 200000 -y 82 -r 1 --b0 1 --mu-exp 4 --mu-division 1.2 --mu-background 11.6 --p-asymmetric 0 --subsample 368 --snapshots 0 29 38 48 63 76 77 81 --seed ${SGE_TASK_ID} --sequential $PATH2SAVE
+${PATH2HSC}/target/release/hsc -c 100000 -y 82 -r 1 --b0 1 --mu-exp 4 --mu-division 1.2 --mu-background 11.6 --p-asymmetric 0 --subsample 368 --snapshots 0 29 38 48 63 76 77 81 --seed ${SGE_TASK_ID} --sequential $PATH2SAVE
 
 echo "end hsc $(date +"%T")"
 
