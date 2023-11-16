5f2f9cb480e078122a1cedab292f10c43ee0414f
Francesco Terenzi <hfx923@qmul.ac.uk>
2023-11-16 11:30:26 +0000

    calibration with different r

--- neutral.sh
+++ neutral.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -pe smp 1
-#$ -l h_vmem=16G
+#$ -l h_vmem=32G
 #$ -j y
 #$ -l h_rt=01:0:0
 #$ -t 1-500
@@ -13,7 +13,11 @@ PATH2SAVE=/data/scratch/hfx923/hsc-draft/${VERSION}/neutral
 
 echo "run hsc seed ${SGE_TASK_ID} ${PATH2SAVE} $(date +"%T")"
 
-${PATH2HSC}/target/release/hsc -c 100000 -y 91 -r 1 --b0 1 --mu-exp 4 --mu-division 1.2 --mu-background 11.6 --p-asymmetric 0 --mu0 1 --neutral --subsample 368 --snapshots 0 10 20 30 40 50 60 70 80 90 --seed ${SGE_TASK_ID} --sequential $PATH2SAVE
+# ${PATH2HSC}/target/release/hsc -c 100000 -y 91 -r 1 --r 4 --mu-exp 4.4 --mu-division 1.2 --mu-background 4.4 --p-asymmetric 0 --mu0 1 --neutral --subsample 368 --snapshots 0 10 20 30 40 50 60 70 80 90 --seed ${SGE_TASK_ID} --sequential $PATH2SAVE/r4
+
+# ${PATH2HSC}/target/release/hsc -c 100000 -y 91 -r 1 --r 0.25 --mu-exp 4.4 --mu-division 1.2 --mu-background 13.4 --p-asymmetric 0 --mu0 1 --neutral --subsample 368 --snapshots 0 10 20 30 40 50 60 70 80 90 --seed ${SGE_TASK_ID} --sequential $PATH2SAVE/r0dot25
+
+${PATH2HSC}/target/release/hsc -c 100000 -y 91 -r 1 --r 1 --mu-exp 4.4 --mu-division 1.2 --mu-background 11.6 --p-asymmetric 0 --mu0 1 --neutral --subsample 368 --snapshots 0 10 20 30 40 50 60 70 80 90 --seed ${SGE_TASK_ID} --sequential $PATH2SAVE/r1
 
 echo "end hsc $(date +"%T")"
 
