a2dabe1dc9f3cf0c18267a67ac47c81ced3e8abf
Francesco Terenzi <hfx923@qmul.ac.uk>
2024-03-04 14:50:46 +0000

    update abc to v4.0.0

--- abc/particles.sh
+++ abc/particles.sh
@@ -13,13 +13,12 @@ PATH2PARAMS=$1
 PATH2HSC=/data/home/hfx923/hsc
 # assume that the maj version nb is just one digit
 VERSION=v$(sed -n '1,4 s/^version = "\([0-9]\.[0-9][0-9]*\.[0-9][0-9]*\)"/\1/p' ${PATH2HSC}/Cargo.toml)
-PATH2SAVE=/data/scratch/hfx923/hsc-draft/${VERSION}
 
 echo "run hsc seed ${SGE_TASK_ID} ${PATH2SAVE} $(date +"%T")"
 INPUT_ARGS=$(sed -n "${SGE_TASK_ID}p" ${PATH2PARAMS})
 
 # the subsamples are taken from Fig1a of Mitchell's paper
-${PATH2HSC}/target/release/hsc $INPUT_ARGS --subsamples 216 390 407 380 362 361 367 451 328 --snapshots 0 0 29 38 48 63 76 77 81 --seed $SGE_TASK_ID --sequential $PATH2SAVE
+${PATH2HSC}/target/release/hsc --seed $SGE_TASK_ID $INPUT_ARGS
 
 echo "end hsc $(date +"%T")"
 
