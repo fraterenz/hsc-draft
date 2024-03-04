715129adeab64d1b50f1595cc0e01313613d4de4
Francesco Terenzi <hfx923@qmul.ac.uk>
2024-03-04 13:54:29 +0000

    update code to hsc v4.0.0

--- sfs/sfs.sh
+++ sfs/sfs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #$ -pe smp 1
-#$ -l h_vmem=16G
+#$ -l h_vmem=32G
 #$ -j y
 #$ -l h_rt=01:0:0
-#$ -t 1-901
+#$ -t 1-900
 #$ -o jobs/$JOB_NAME.$JOB_ID.$TASK_ID.out
 if [ "$#" -ne 1 ]; then
     echo "Illegal number of arguments: must provide one parameters.txt file"
@@ -13,12 +13,11 @@ PATH2PARAMS=$1
 PATH2HSC=/data/home/hfx923/hsc
 # assume that the maj version nb is just one digit
 VERSION=v$(sed -n '1,4 s/^version = "\([0-9]\.[0-9][0-9]*\.[0-9][0-9]*\)"/\1/p' ${PATH2HSC}/Cargo.toml)
-PATH2SAVE=/data/home/hfx923/hsc-draft/sfs/${VERSION}
 
-echo "run hsc seed ${SGE_TASK_ID} ${PATH2SAVE} $(date +"%T")"
+echo "run hsc ${VERSION} seed ${SGE_TASK_ID} $(date +"%T")"
 INPUT_ARGS=$(sed -n "${SGE_TASK_ID}p" ${PATH2PARAMS})
 
-${PATH2HSC}/target/release/hsc $INPUT_ARGS --seed $SGE_TASK_ID $PATH2SAVE
+${PATH2HSC}/target/release/hsc --seed $SGE_TASK_ID $INPUT_ARGS 
 
 echo "end hsc $(date +"%T")"
 
